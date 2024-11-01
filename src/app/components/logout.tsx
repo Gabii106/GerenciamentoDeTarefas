f9263c42f2ec1dafb4f49863ea7d030ddb413476
MIYUKI ARAÚJO HIRATA <miyuki_hirata@estudante.sesisenai.org.br>
2024-11-01 20:44:39 -0300

    login

---
+++ src/app/components/logout.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { signOut } from 'firebase/auth';
+import { auth } from '../lib/firebaseconfig' ;
+import { useRouter } from 'next/navigation';
+import { useAuth } from '../hooks/userAuth';
+
+export default function LogOut() {
+    const { user } = useAuth(); // Acessa o usuário autenticado
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            router.push('/'); // Redireciona para a página inicial após o logout
+        } catch (error) {
+         console.error('Erro ao sair:', error);
+        }
+     };
+
+     return (
+        <div className="flex items-center justify-between bg-gray-700 md:bg-gray-100
+    mb-0 md:mb-3">
+            <h1 className="text-sm text-white md:text-black">
+                Bem-vindo, {user?.email}!
+            </h1>
+            <button
+                onClick={handleLogout}
+                className="ml-4 bg-red-500 text-white text-sm py-2 px-4 rounded
+    hover:bg-red-600 transition duration-150"
+                >
+                    Sair
+            </button>
+        </div>
+        );
+    }
+        
---
+++ src/app/inicio/page.tsx
@@ -0,0 +1,60 @@
+"use client"; // Adicione esta linha
+import { useAuth } from '../hooks/userAuth';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import LogOut from '../components/logout';
+
+
+export default function Turmas() {
+const { user, loading } = useAuth();
+const router = useRouter();
+
+if (loading) return <p>Loading...</p>; // Exibir um carregamento
+
+if (!user) {
+    router.push('/login'); // Redirecionar para a página de login se não estiver autenticado
+return null;
+}
+
+return (
+    <div className="min-h-screen bg-gray-100 p-0 md:p-2">
+        <LogOut />
+        <hr />
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8 mt-2">
+Gerenciamento de Turmas</h1>
+
+    {/* Seção de Botões */}
+    <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+        {/* Botão Cadastrar Turmas */}
+        <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
+            <UsersIcon className="h-12 w-12 text-blue-500 mb-4" /> {/* Ícone de
+pessoas */}
+            <Link href="/turmas/cadastro">
+                <button
+                    className="bg-blue-500 text-white py-3 px-6 rounded hover:bg-blue-700
+focus:outline-none focus:bg-blue-700 transition duration-150"
+        >  
+
+            Cadastrar Turmas e Alunos
+        </button>
+    </Link>
+</div>
+        {/* Botão Relatórios de Turmas */}
+        <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col
+items-center">
+            <ChartBarIcon className="h-12 w-12 text-green-600 mb-4" /> {/* Ícone de
+gráfico */}
+            <Link href="/turmas/relatorio">
+                <button
+                    className="bg-green-600 text-white py-3 px-6 rounded
+hover:bg-green-800 focus:outline-none focus:bg-green-800 transition duration-150"
+            >
+                    Relatórios de Turmas e Alunos
+                </button>
+            </Link>
+        </div>
+    </div>
+</div>
+);
+}
+ 
